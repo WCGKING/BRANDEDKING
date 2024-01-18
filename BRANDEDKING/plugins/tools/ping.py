5aeb57a345f5f7379153383a9798f62eb011c7af
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-18 20:12:16 +0530

    𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚

--- BRANDEDKING/plugins/tools/ping.py
+++ BRANDEDKING/plugins/tools/ping.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from BRANDEDKING import app
-from BRANDEDKING.core.call import TGN
+from BRANDEDKING.core.call import BRANDED
 from BRANDEDKING.utils import bot_sys_stats
 from BRANDEDKING.utils.decorators.language import language
 from BRANDEDKING.utils.inline import supp_markup
