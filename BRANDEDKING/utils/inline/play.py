980d88501d136fcaedf01ee9ed64ecd055dfcd68
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-18 20:42:57 +0530

    𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚

--- BRANDEDKING/utils/inline/play.py
+++ BRANDEDKING/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from TGNMusic.utils.formatters import time_to_seconds
+from BRANDEDKING.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
