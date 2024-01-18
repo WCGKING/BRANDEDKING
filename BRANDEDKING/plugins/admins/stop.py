fafbe6e5c3e138e383ee6451cb762e6999804fc9
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-18 19:17:14 +0530

    𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚

--- BRANDEDKING/plugins/admins/stop.py
+++ BRANDEDKING/plugins/admins/stop.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from BRANDEDKING import app
-from BRANDEDKING.core.call import TGN
+from BRANDEDKING.core.call import BRANDED
 from BRANDEDKING.utils.database import set_loop
 from BRANDEDKING.utils.decorators import AdminRightsCheck
 from BRANDEDKING.utils.inline import close_markup
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await TGN.stop_stream(chat_id)
+    await BRANDED.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
