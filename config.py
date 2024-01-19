b8d9741d67145c12c958cdcc713562bb7b7ce690
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-19 18:22:46 +0530

    𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WCGKING/BRANDED-X-KING",
+    "https://github.com/WCGKING/BRANDEDKING",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
