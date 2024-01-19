b4ea3b879c0ecb02ba67b606f14f0b22ee5d1d25
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-01-19 17:40:55 +0530

    𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚

--- BRANDEDKING/plugins/play/play.py
+++ BRANDEDKING/plugins/play/play.py
@@ -288,7 +288,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await TGN.stream_call(url)
+                await BRANDED.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
