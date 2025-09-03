b6e89cdf0d006c51532a0fd07b4ed4d34cd0a0a1
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-09-03 13:17:31 +0530

    Update Youtube.py

--- BRANDEDKING/platforms/Youtube.py
+++ BRANDEDKING/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from BRANDEDKING.utils.database import is_on_off
+from BRANDEDKING.utils.formatters import time_to_seconds
 
 
 
