00110ba75b94a1788639e902e3821ae91e3f2a6a
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2025-12-08 13:01:25 +0530

    Update Youtube.py

--- BRANDEDKING/platforms/Youtube.py
+++ BRANDEDKING/platforms/Youtube.py
@@ -16,7 +16,11 @@ import random
 import logging
 import aiohttp
 import config
-from config import API_URL, VIDEO_API_URL, API_KEY
+import traceback
+from BRANDEDKING import LOGGER
+
+API_URL = "https://teaminflex.xyz"  # Change to your API server URL
+API_KEY = "INFLEX93454428D"
 
 
 def cookie_txt_file():
