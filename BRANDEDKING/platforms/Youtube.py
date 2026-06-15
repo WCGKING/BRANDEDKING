bd2d8c72f70fec8f0ab3ca7bffa81f3932722dd8
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2026-06-15 10:45:02 +0530

    Update Youtube.py

--- BRANDEDKING/platforms/Youtube.py
+++ BRANDEDKING/platforms/Youtube.py
@@ -14,9 +14,6 @@ API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsWtQUKDTAv5KUMyByWwFe") ## 
 
 DOWNLOAD_DIR = "downloads"
 
-DOWNLOAD_DIR = "downloads"
-
-
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
